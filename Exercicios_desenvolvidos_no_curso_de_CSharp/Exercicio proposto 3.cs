7e32769c2dadad9e24587bad9c408309719272f9
altai <altairxneto@gmail.com>
2022-06-28 22:51:49 -0300

    Criação exercicio proposto 3

---
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Exercicio proposto 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioProposto3 {
+    public class ExercicioP3 {
+        public void ExP3() {
+            Console.WriteLine("Digite o valor das variáveis A, B, C e D para efetuar a diferença do produto de A e B por C e D: ");
+
+            Console.WriteLine("Valor A:");
+            int valorA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor B:");
+            int valorB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor C:");
+            int valorC = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor D:");
+            int valorD = int.Parse(Console.ReadLine());
+
+            int resultado = valorA*valorB - valorC*valorD;
+
+            Console.WriteLine("Diferenca = "+resultado);
+        }
+    }
+}
