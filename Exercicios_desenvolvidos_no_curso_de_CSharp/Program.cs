977a4ca0b6709b0ac69b8b4d8d0ae1c63b1d3ed2
altai <altairxneto@gmail.com>
2022-06-28 22:34:06 -0300

    Implementação exercicio proposto1

--- Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
@@ -1,16 +1,21 @@
 ﻿using PrimeiroExercicio;
 using SegundoExercicio;
+using ExercicioProposto1;
 
 namespace Exercicios_desenvolvidos_no_curso_de_CSharp {
     public class Program {
         public static void Main(string[] args) {
-            //Exercicio1 exercicio1 = new Exercicio1();
+            //Exercicio1 exercicio = new Exercicio1();
 
-            //exercicio1.Ex1();
+            //exercicio.Ex1();
 
-            //Exercicio2 exercicio2 = new Exercicio2();
+            //Exercicio2 exercicio = new Exercicio2();
 
-            //exercicio2.Ex2();
+            //exercicio.Ex2();
+
+            ExercicioP1 exercicio = new ExercicioP1();
+
+            exercicio.ExP1();
         }
     }
 }
