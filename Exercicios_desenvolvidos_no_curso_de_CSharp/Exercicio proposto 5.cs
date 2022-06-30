590f49d182cd91d3f289ab4eee4b5143596befd4
altai <altairxneto@gmail.com>
2022-06-30 16:23:55 -0300

    Criação do exercicio proposto 5

---
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Exercicio proposto 5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto5 {
+    public class ExercicioP5 {
+        public void ExP5() {
+            Console.WriteLine("Detalhes peça 1. Digite o código da peça, o número de peças e o valor de cada peça:");
+            string peca1 = Console.ReadLine();
+
+            Console.WriteLine("Detalhes peça 2. Digite o código da peça, o número de peças e o valor de cada peça:");
+            string peca2 = Console.ReadLine();
+
+            string[] peca1Split = peca1.Split(' ');
+            string[] peca2Split = peca2.Split(' ');
+
+            int codigoPeca1 = int.Parse(peca1Split[0]);
+            int numeroPeca1 = int.Parse(peca1Split[1]);
+            double valorPeca1 = double.Parse(peca1Split[2],CultureInfo.InvariantCulture);
+
+            int codigoPeca2 = int.Parse(peca2Split[0]);
+            int numeroPeca2 = int.Parse(peca2Split[1]);
+            double valorPeca2 = double.Parse(peca2Split[2], CultureInfo.InvariantCulture);
+
+            double valorPagar = valorPeca1*numeroPeca1 + valorPeca2*numeroPeca2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ "+valorPagar.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
