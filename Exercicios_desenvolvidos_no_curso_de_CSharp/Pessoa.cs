a30f2fe290b3fe865a5cb78b1ed0ea55f17c824e
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-02 13:54:47 -0300

    criei a classe Pessoa

---
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_desenvolvidos_no_curso_de_CSharp {
+    internal class Pessoa {
+    }
+}
