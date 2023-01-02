fdb4de3c1fa279aef63b3f4beb07b3a2d1c4f8ea
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-02 13:16:32 -0300

    comentei o exercicio 1

--- Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
@@ -5,8 +5,8 @@ namespace Exercicios_desenvolvidos_no_curso_de_CSharp {
     public class Program {
         public static void Main(string[] args) {
 
-            Exercicio1 exercicio = new Exercicio1();
-            exercicio.Ex1();
+            //Exercicio1 exercicio = new Exercicio1();
+            //exercicio.Ex1();
 
             //Exercicio2 exercicio = new Exercicio2();
             //exercicio.Ex2();
