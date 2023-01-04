b65a08b9d85f0077ca0fabde56e81e1f6dd83207
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-04 15:44:04 -0300

    Fiz pequenas alterações irrelevantes
    Apenas mudei a forma de comentar o código no método main

--- Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/Program.cs
@@ -5,44 +5,46 @@ namespace Exercicios_desenvolvidos_no_curso_de_CSharp {
     public class Program {
         public static void Main(string[] args) {
 
-            //ExercicioF1 exercicio = new ExercicioF1();
-            //exercicio.ExF1();
+            /*
+            ExercicioF1 exercicio = new ExercicioF1();
+            exercicio.ExF1();
 
-            //ExercicioF2 exercicio = new ExercicioF2();
-            //exercicio.ExF2();
+            ExercicioF2 exercicio = new ExercicioF2();
+            exercicio.ExF2();
 
-            //ExercicioF3 exercicio = new ExercicioF3();
-            //exercicio.ExF3();
+            ExercicioF3 exercicio = new ExercicioF3();
+            exercicio.ExF3();
 
-            //ExercicioP1 exercicio = new ExercicioP1();
-            //exercicio.ExP1();
+            ExercicioP1 exercicio = new ExercicioP1();
+            exercicio.ExP1();
 
-            //ExercicioP2 exercicio = new ExercicioP2();
-            //exercicio.ExP2();
+            ExercicioP2 exercicio = new ExercicioP2();
+            exercicio.ExP2();
 
-            //ExercicioP3 exercicio = new ExercicioP3();
-            //exercicio.ExP3();
+            ExercicioP3 exercicio = new ExercicioP3();
+            exercicio.ExP3();
 
-            //ExercicioP4 exercicio = new ExercicioP4();
-            //exercicio.ExP4();
+            ExercicioP4 exercicio = new ExercicioP4();
+            exercicio.ExP4();
 
-            //ExercicioP5 exercicio = new ExercicioP5();
-            //exercicio.ExP5();
+            ExercicioP5 exercicio = new ExercicioP5();
+            exercicio.ExP5();
 
-            //ExercicioP6 exercicio = new ExercicioP6();
-            //exercicio.ExP6();
+            ExercicioP6 exercicio = new ExercicioP6();
+            exercicio.ExP6();
 
-            //ExercicioP7 exercicio = new ExercicioP7();
-            //exercicio.ExP7();
+            ExercicioP7 exercicio = new ExercicioP7();
+            exercicio.ExP7();
 
-            //ExercicioP8 exercicio = new ExercicioP8();
-            //exercicio.ExP8();
+            ExercicioP8 exercicio = new ExercicioP8();
+            exercicio.ExP8();
 
-            //ExercicioP9 exercicio = new ExercicioP9();
-            //exercicio.ExP9();
+            ExercicioP9 exercicio = new ExercicioP9();
+            exercicio.ExP9();
 
             ExercicioP10 exercicio = new ExercicioP10();
             exercicio.ExP10();
+            */
         }
     }
 }
