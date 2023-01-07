5ae5caa05c53bbd5e14ba8ab443de0c52a4da0c5
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-07 19:28:16 -0300

    Create ContaBancariaExF4.cs

---
+++ Exercicios_desenvolvidos_no_curso_de_CSharp/ContaBancariaExF4.cs
@@ -0,0 +1,15 @@
+﻿namespace ExercicioFixacao {
+    class ContaBancariaExF4 {
+        private double _saldoConta;
+        public string NomeTitular;
+        public int NumeroConta { get; private set; }
+
+        public void Deposito(double valor) {
+            _saldoConta += valor;
+        }
+
+        public void Saque(double valor) {
+            _saldoConta -= valor;
+        }
+    }
+}
