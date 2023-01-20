ae36fb0821f201884f0f5400e7b343fcf0b1d2bc
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-20 17:07:17 -0300

    Implementei a classe PessoaFisica do ProjetoContribuintesDeImpostos

---
+++ Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoContribuintesDeImpostos/Entities/PessoaFisica.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ProjetoContribuintesDeImpostos.Entities {
+    class PessoaFisica:Contribuintes {
+        public double GastosSaude { get; set; }
+
+        public PessoaFisica() { }
+
+        public PessoaFisica(string nome, double rendaAnual, double gastosSaude):base(nome, rendaAnual) {
+            GastosSaude = gastosSaude;
+        }
+
+        public override double ImpostoPago() {
+            double impostos = 0;
+            if(RendaAnual < 20000) {
+                impostos = RendaAnual * 15 / 100;
+            }
+            else if(RendaAnual < 20000 && GastosSaude != 0) {
+                impostos = (RendaAnual * 15 / 100) - (GastosSaude/2);
+            }
+            else if (RendaAnual > 20000) {
+                impostos = RendaAnual * 25 / 100;
+            }
+            else if (RendaAnual > 20000 && GastosSaude != 0) {
+                impostos = (RendaAnual * 25 / 100) - (GastosSaude / 2);
+            }
+
+            return impostos;
+        }
+    }
+}
