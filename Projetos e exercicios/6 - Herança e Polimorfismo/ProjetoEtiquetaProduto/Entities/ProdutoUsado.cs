930669b6d737097c64182a0092e46f4e57867a6f
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-20 14:39:44 -0300

    Criei a classe ProdutoUsado do ProjetoEtiquetaProduto

---
+++ Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoEtiquetaProduto/Entities/ProdutoUsado.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ProjetoEtiquetaProduto.Entities {
+    class ProdutoUsado:Produto{
+        public DateTime DataFabricado { get; set; }
+
+        public ProdutoUsado() { }
+
+        public ProdutoUsado(string name, double price, DateTime dataFabricado):base(name, price) {
+            DataFabricado = dataFabricado;
+        }
+
+        public override string PriceTag() {
+            return Name + " (used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + " (Manufacture date: " + DataFabricado.ToShortDateString + ")";
+        }
+    }
+}
