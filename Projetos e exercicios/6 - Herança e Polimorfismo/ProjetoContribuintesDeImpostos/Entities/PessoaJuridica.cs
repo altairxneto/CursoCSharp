62442f47b4a302c4308e7fe0aefa370f1c8f6032
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-20 17:11:26 -0300

    Implementei a classe PessoaJuridica

--- Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoContribuintesDeImpostos/Entities/PessoaJuridica.cs
+++ Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoContribuintesDeImpostos/Entities/PessoaJuridica.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProjetoContribuintesDeImpostos.Entities {
-    internal class PessoaJuridica {
+    class PessoaJuridica:Contribuintes {
+        public int NumeroFuncionarios { get; set; }
+
+        public PessoaJuridica() { }
+
+        public PessoaJuridica(string nome, double rendaAnual, int numeroFuncionarios) : base(nome, rendaAnual) {
+            NumeroFuncionarios = numeroFuncionarios;
+        }
+
+        public override double ImpostoPago() {
+            double impostos = 0;
+
+            if(NumeroFuncionarios > 10) {
+                impostos = RendaAnual * 14 / 100;
+            }
+            else {
+                impostos = RendaAnual * 16 / 100;
+            }
+
+            return impostos;
+        }
     }
 }
