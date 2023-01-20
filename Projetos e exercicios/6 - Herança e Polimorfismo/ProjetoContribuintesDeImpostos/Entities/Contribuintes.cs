3ab8ceb94d010099efe7b65a1a54d6114a9e9675
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-01-20 17:12:35 -0300

    Implementei e finalizei a classe Contribuintes

--- Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoContribuintesDeImpostos/Entities/Contribuintes.cs
+++ Projetos e exercicios/6 - Herança e Polimorfismo/ProjetoContribuintesDeImpostos/Entities/Contribuintes.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProjetoContribuintesDeImpostos.Entities {
-    internal class Contribuintes {
+    abstract class Contribuintes {
+        public string Nome { get; set; }
+        public double RendaAnual { get; set; }
+
+        public Contribuintes() {
+        }
+
+        protected Contribuintes(string nome, double rendaAnual) {
+            Nome = nome;
+            RendaAnual = rendaAnual;
+        }
+
+        public abstract double ImpostoPago();
     }
 }
