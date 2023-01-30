09e4dd91368986e01393f67518b8540c8fea2d3c
Altair Neto <altairxneto@gmail.com>
2023-01-30 10:33:55 -0300

    Implementei a classe program com a solução do projeto de manipular arquivos

--- Projetos e exercicios/8 - Trabalhando com arquivos/ProjetoManipularArquivosItensVendidos/Program.cs
+++ Projetos e exercicios/8 - Trabalhando com arquivos/ProjetoManipularArquivosItensVendidos/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using Projeto.Entities;
 
 namespace Projeto {
@@ -9,9 +8,27 @@ namespace Projeto {
                 Console.WriteLine("Digite o path que está seu arquivo:");
                 string pathOrigem = Console.ReadLine();
 
+                Directory.CreateDirectory(Path.GetDirectoryName(pathOrigem) + @"\out");
+
                 string pathDestino = Path.GetDirectoryName(pathOrigem) + @"\out\summary.csv";
 
+                string[] lerArquivos = File.ReadAllLines(pathOrigem);
 
+                StreamWriter sr = null;
+
+                foreach (string line in lerArquivos) {
+                    string[] auxiliar = line.Split(',');
+
+                    string nome = auxiliar[0];
+                    double preco = double.Parse(auxiliar[1], CultureInfo.InvariantCulture);
+                    int quantidade = int.Parse(auxiliar[2]);
+
+                    Produto produto = new Produto(nome, preco, quantidade);
+
+                    using(sr = File.AppendText(pathDestino)) {
+                        sr.WriteLine(produto.Nome + "," + produto.ValorTotal().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
             }
             catch(IOException excecao) {
                 Console.WriteLine("Ocorreu um erro!");
