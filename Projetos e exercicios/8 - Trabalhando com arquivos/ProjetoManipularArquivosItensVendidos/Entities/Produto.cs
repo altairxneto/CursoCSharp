114a2b52728b3c9e9c10088592edc2657ea88e48
Altair Neto <altairxneto@gmail.com>
2023-01-30 10:05:18 -0300

    Implementei a classe Produto

---
+++ Projetos e exercicios/8 - Trabalhando com arquivos/ProjetoManipularArquivosItensVendidos/Entities/Produto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Projeto.Entities {
+    public class Produto {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotal() {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ Projetos e exercicios/8 - Trabalhando com arquivos/ProjetoManipularArquivosItensVendidos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Projeto.Entities;
+
+namespace Projeto {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Console.WriteLine("Digite o path que está seu arquivo:");
+                string pathOrigem = Console.ReadLine();
+
+                string pathDestino = Path.GetDirectoryName(pathOrigem) + @"\out\summary.csv";
+
+
+            }
+            catch(IOException excecao) {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(excecao.Message);
+            }
+
+
+
+        }
+    }
+}
