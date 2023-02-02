805befbb9c91a7c46fad6f47b241b4efb0396fd7
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-02-02 16:27:08 -0300

    Implementei o service ServicoDeContrato

---
+++ Projetos e exercicios/9 - Interface/ProjetoCadastroDeContrato/Services/ServicoDeContrato.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Projeto.Services {
+    class ServicoDeContrato {
+        private IServicoDePagamentoOnline _servicoPagamentoOnline;
+
+        public ServicoDeContrato() { }
+
+        public ServicoDeContrato(IServicoDePagamentoOnline servicoPagamentoOnline) {
+            _servicoPagamentoOnline = servicoPagamentoOnline;
+        }
+    }
+}
