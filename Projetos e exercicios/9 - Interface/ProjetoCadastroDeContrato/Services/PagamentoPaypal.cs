fa147572f6abd25e1bfa7687abff149d7d5295ca
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-02-02 16:18:03 -0300

    Terminei de implementar o PagamentoPaypal

--- Projetos e exercicios/9 - Interface/ProjetoCadastroDeContrato/Services/PagamentoPaypal.cs
+++ Projetos e exercicios/9 - Interface/ProjetoCadastroDeContrato/Services/PagamentoPaypal.cs
@@ -1,11 +1,11 @@
 ﻿
 namespace Projeto.Services {
     class PagamentoPaypal : IServicoDePagamentoOnline {
-        public double JurosDePagamento(double valor, int meses) {
-            return valor + valor * 0.01 * meses;
+        public double JurosDePagamento(double valor, int mes) {
+            return valor + valor * 0.01 * mes;
         }
         public double TaxaDePagamento(double valor) {
-            return
+            return valor + (valor * 0.02);
         }
     }
 }
