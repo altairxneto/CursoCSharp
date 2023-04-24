ed46835c36d34c6f23e6b75cd865e423b535e64b
Altair Neto <altairxneto@gmail.com>
2023-04-24 16:23:13 -0300

    O arquivo que será usado para a leitura foi criado e o código que irá ler o arquivo e solucionar o problema foi implementado

--- Projetos e exercicios/11 - LINQ e Expressao Lambda/ProjetoLinqDadosFuncionario/ProjetoLinqDadosFuncionario/Program.cs
+++ Projetos e exercicios/11 - LINQ e Expressao Lambda/ProjetoLinqDadosFuncionario/ProjetoLinqDadosFuncionario/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projeto.Entidades;
+using System.Globalization;
+namespace Projeto {
+    class Program {
+        public static void Main() {
+            Console.WriteLine("Insira o path do arquivo: ");
+            string pathArquivo = Console.ReadLine() + @"\funcionarios.csv";
+
+            Console.WriteLine("Insira o salário: ");
+            double salarioFuncionarioReferencia = double.Parse(Console.ReadLine());
+
+            string[] arrayArquivoFuncionarios = File.ReadAllLines(pathArquivo);
+
+            List<Funcionario> funcionarios = new();
+            foreach(string line in arrayArquivoFuncionarios) {
+                string[] splitDaString = line.Split(",");
+
+                funcionarios.Add(new Funcionario(splitDaString[0], splitDaString[1], double.Parse(splitDaString[2], CultureInfo.InvariantCulture)));
+            }
+
+            var emailsFuncionarios = funcionarios.Where(p => p.Salario > salarioFuncionarioReferencia).OrderBy(p => p.Email).Select(p => p.Email);
+
+            foreach(var emailFuncionario in emailsFuncionarios) {
+                Console.WriteLine(emailFuncionario.ToString());
+            }
+
+            var salarioPessoas = funcionarios.Where(p => p.Nome[0].ToString() == "M").Select(p => p.Salario).Sum();
+
+            Console.WriteLine("Soma dos salários das pessoas que tenham o nome que começa com a letra M: R$" + salarioPessoas);
+        }
+    }
+}
